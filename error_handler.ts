15b5217eaec55cf6e167a438cbfe2cd876c1b2f6
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2023-03-06 14:21:23 +0100

    Removing return statement from the error handler

--- error_handler.ts
+++ error_handler.ts
@@ -3,7 +3,6 @@ import AppError from "./AppError.ts";
 export default function (err: AppError) {
   if (err.error instanceof Deno.errors.NotFound) {
     console.warn(`No entry was found at this path: ${err.path}`);
-    return;
   } else {
     console.log("Something went wrong. PowerDelete is exiting...");
   }
